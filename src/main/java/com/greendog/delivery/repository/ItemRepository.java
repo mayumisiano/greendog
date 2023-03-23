22b324ac3f50b732a6813fe67ed52f81fede478e
Paula Siano <Paula_Siano@mckinsey.com>
2023-03-23 16:50:09 -0300

    feat:  created client, order and item repositories

---
+++ src/main/java/com/greendog/delivery/repository/ItemRepository.java
@@ -0,0 +1,9 @@
+package com.greendog.delivery.repository;
+
+import com.greendog.delivery.model.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
+}
---
+++ src/main/java/com/greendog/delivery/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.greendog.delivery.repository;
+
+import com.greendog.delivery.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
